ad425edf89fc56098d880a793eca0b1f475161a7
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-09-07 15:10:26 +0700

    initial commit

---
+++ src/HitungFibonaci/Tugas01.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HitungFibonaci;
+
+/**
+ *
+ * @author Anandarafi
+ */
+public class Tugas01 {
+    private static void tampilJudl(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonaci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        String identitas = "Ananda Rafi Amanullah / X RPL 3 / 04";
+        
+        tampilJudul(identitas);
+    }
+}
