1977a8b534804a28bcf6606d061a28fc0d4840ba
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-09-07 15:30:00 +0700

    Tam bah ha s i l

--- src/HitungFibonaci/Tugas01.java
+++ src/HitungFibonaci/Tugas01.java
@@ -32,6 +32,10 @@ public class Tugas01 {
         }
         return hasil[n-1];
     }
+    private static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil);
+    }
     public static void main(String[] args) {
         String identitas = "Ananda Rafi Amanullah / X RPL 3 / 04";
         
@@ -40,6 +44,8 @@ public class Tugas01 {
         int n = tampilInput();
         
         BigInteger hasil = fibo(n);
+        
+                tampilHasil(n, hasil);
        
     }
 }
