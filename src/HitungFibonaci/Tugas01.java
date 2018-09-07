877f57486f7a9a3b5b3d10249e2496c86cd2929e
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-09-07 15:12:46 +0700

    Tam bah Input

--- src/HitungFibonaci/Tugas01.java
+++ src/HitungFibonaci/Tugas01.java
@@ -4,20 +4,28 @@
  * and open the template in the editor.
  */
 package HitungFibonaci;
-
+import java.util.Scanner;
 /**
  *
  * @author Anandarafi
  */
 public class Tugas01 {
-    private static void tampilJudl(String identitas)
+    private static void tampilJudul(String identitas)
     {
         System.out.println("Identitas : " + identitas);
         
         System.out.println("\nHitung Fibonaci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
     }
-    
+    private static int tampilInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke : ");
+        int n = scanner.nextInt();
+        
+        return n;
+    }
     
     
     
@@ -26,5 +34,7 @@ public class Tugas01 {
         String identitas = "Ananda Rafi Amanullah / X RPL 3 / 04";
         
         tampilJudul(identitas);
+        
+        int n = tampilInput();
     }
 }
