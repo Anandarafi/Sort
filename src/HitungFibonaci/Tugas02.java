74b64b6d87a0ab20f8d6874788c454bc1dd34e9b
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-09-14 12:18:54 +0700

    Tam bah an

---
+++ src/HitungFibonaci/Tugas02.java
@@ -0,0 +1,36 @@
+
+package HitungFibonaci;
+import java.util.*;
+public class Tugas02 {
+    public static void main(String[] args) {
+        System.out.println("Pecahannya adalah Rp 5.000, Rp 2.000, Rp 1.000, Rp 500, Rp 100");
+//        int uang = input();
+        int[] Nominal = new int[]{5000, 2000, 1000, 500, 100};
+        
+//        JumlahUang(uang, Nominal);
+    }
+//    private static int input()
+//    {
+//       Scanner baca = new Scanner(System.in);
+//       
+//       System.out.print("Jumlah Uang = Rp ");
+//       int uang = baca.nextInt();
+//       
+//       return uang;
+//    }
+//    private static void JumlahUang(int uang, int[] Nominal)
+//    {
+//        for(int i = 0; i < Nominal.length; i++)
+//        {
+//            int n = 0;
+//            while(uang >= Nominal[i])
+//            {
+//                uang = uang - Nominal[i];
+//                n++;
+//            }
+//            System.out.println
+//        (Nominal[i] + " Sebanyak " + n + " lembar");
+//        }
+//    }
+ 
+}
